e2c8bf6b9d9ff13dce3ffab70d3a3a4aed538070
Anshu Pal <64951054+anshuUnity@users.noreply.github.com>
2023-09-05 11:14:25 +0530

    message sending added

--- chat/consumer.py
+++ chat/consumer.py
@@ -18,4 +18,25 @@ class PersonalChatConsumer(AsyncWebsocketConsumer):
             )
             await self.accept()
 
-    
+    async def receive(self, text_data=None, bytes_data=None):
+        data = json.loads(text_data)
+        message = data['message']
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                "type": "chat_message",
+                "message": message
+            }
+        )
+
+    async def chat_message(self, event):
+        message = event['message']
+        await self.send(text_data=json.dumps({
+            "message":message
+        }))
+
+    async def disconnect(self, code):
+        self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
